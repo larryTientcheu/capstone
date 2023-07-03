9284c45969fd2bc0089c8e7e90d5e0da7831b4c8
larry <larrytientcheu1@gmail.com>
2023-07-03 22:32:17 +0200

    setting up fmono

---
+++ api-restaurant/src/index.ts
@@ -0,0 +1,16 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+// const port = process.env.PORT;
+const port = 5000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server is running.');
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
