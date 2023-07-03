c2eaa694e2eb10ed6231c22d70e1e27d5625b6e9
larry <larrytientcheu1@gmail.com>
2023-07-03 22:34:32 +0200

    fix port

--- api-restaurant/src/index.ts
+++ api-restaurant/src/index.ts
@@ -4,8 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app: Express = express();
-// const port = process.env.PORT;
-const port = 5000;
+const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server is running.');
